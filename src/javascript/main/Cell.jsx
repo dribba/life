3a8623ef879633406216e398d699dd9e21232bef
David Ribba <dribba@gmail.com>
2016-02-06 13:14:17 -0500

    Update grid build function, improve grid rendering, improve styles, add stepper button

--- src/javascript/main/Cell.jsx
+++ src/javascript/main/Cell.jsx
@@ -1,3 +1,12 @@
-export function Cell(props) {
-  return <div className={"cell " + props.alive ? "alive" : "dead"}></div>
+import React from 'react';
+
+export default function Cell(props) {
+  let classes = 'cell ' + (props.alive ? 'alive' : 'dead');
+  let pos = {
+    top: props.y * 10,
+    left: props.x * 10
+  }
+
+
+  return <div className={classes} style={pos}></div>
 }
--- src/javascript/main/index.jsx
+++ src/javascript/main/index.jsx
@@ -1,22 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import R from 'ramda';
+import Cell from './Cell.jsx';
 
-//+ _makeRow :: ((Int, Int) -> a) -> Int -> Int -> Array a
-const _makeRow = R.curry((builder, size, colIdx) => {
-  return R.times(i => builder(i, colIdx), size)
-});
 //+ _makeGrid :: Int -> Int -> ((Int, Int) -> a) -> Array (Array a)
 const _makeGrid = R.curry((x, y, builder) => {
-  return R.times(_makeRow(builder, x), y);
+  let grid = [];
+
+  for(let i = 0; i < x; i++) {
+    grid[i] = [];
+    for (let j = 0; j < y; j++) {
+      grid[i][j] = builder(i, j);
+    }
+  }
+
+  return grid;
 });
+const allDead = (x, y) => {
+  return {alive: 0, x, y};
+};
+
+const allAlive = (x, y) => {
+  return {alive: 1, x, y};
+};
+
+const allRandom = (x, y) => {
+  let alive = Math.random() >= 0.5;
+  return {alive: +alive, x, y};
+};
+
+const theGrid = _makeGrid(50, 50, allRandom);
+
+function renderGrid(render, grid) {
+  const renderRow = R.map(render);
+  const renderColumns = R.map(renderRow);
+
+  return renderColumns(grid);
+}
+
+function noop() {};
+
+function renderCell(state) {
+  return (
+    <Cell
+      x={state.x}
+      y={state.y}
+      alive={state.alive}
+      key={`${state.x}-${state.y}`} />
+  );
+}
 
 const Main = () => {
-  let allDead = (x, y) => 0;
+  let cells = renderGrid(renderCell, theGrid);
 
-  console.log(_makeGrid(7, 7, allDead));
-
-  return <div>Hello world!</div>;
+  return (
+    <div id="main-container">
+      <div id="game-container">{cells}</div>
+      <button onClick={noop} type="button">Step</button>
+    </div>
+  );
 }
 
 
